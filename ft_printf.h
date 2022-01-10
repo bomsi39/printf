07a1d24d2cdd1f710522e75ccc3afe936b297961
bomsi39 <damfranke@gmail.com>
2022-01-10 01:35:53 +0100

    REFRESHED HEADER

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dfranke <dfranke@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 14:55:34 by dfranke           #+#    #+#             */
-/*   Updated: 2022/01/10 01:28:13 by dfranke          ###   ########.fr       */
+/*   Updated: 2022/01/10 01:34:53 by dfranke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,19 @@
 # define B_16 16
 # define NO_FLAG 0
 
+// MAIN PRINTF FUNCTIONS
+
 int			ft_printf(const char *str, ...);
 void		ft_format(char c, va_list *arg, int *len);
+
+// PUT FUNTIONS
+
 int			ft_putchar_ret(char c);
 size_t		ft_putstr_ret(char *s);
 int			ft_putascii(ssize_t n, int base, char c, char a);
+
+// UTIL FUNCTIONS
+
 char		*ft_itoa_base(unsigned long long n, int base, char c);
 int			digit(unsigned long long n, int base);
 
